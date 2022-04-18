566cea5161b994dc8898dc9749d06b4e0b928b0b
EuViniciuslima <vinicius.lima.114@ufrn.edu.br>
2022-04-17 21:45:42 -0300

    incrementação tela CONTATOS. interação com icones

--- main.js
+++ main.js
@@ -42,3 +42,21 @@ const swiper = new Swiper('.swiper-container', {
   mousewheel: true,
   keyboard: true
 })
+
+/* Scrollreveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .link
+  `,
+  { interval: 100 }
+)
